62b7a627fe22bd3ceb0ac5d8ef69d34350d7fc2d
DevReis <72721650+DevReis@users.noreply.github.com>
2026-04-06 08:27:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 app.get('/webhook', (req, res) => {
+  const verify_token = 'tokenok'; // O mesmo que você digitou na imagem
+
+  // Parâmetros enviados pela Meta
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
 
-  // O VERIFY_TOKEN deve ser o mesmo que você definirá no Facebook
-  if (mode === 'subscribe' && token === process.env.VERIFY_TOKEN) {
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
+  if (mode && token) {
+    if (mode === 'subscribe' && token === verify_token) {
+      console.log('WEBHOOK_VERIFIED');
+      // RETORNE APENAS O CHALLENGE COM STATUS 200
+      return res.status(200).send(challenge); 
+    } else {
+      return res.sendStatus(403);
+    }
   }
 });
